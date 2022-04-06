d860131ffce0b0123839b6c741ab26342a93466f
Batuhan UZUNOGLU (BilgeAdam) <b.uzunoglu@bilgeadam.com>
2022-04-06 15:44:34 +0300

    [fix] Remove unneccessary usings

--- src/Domain/Entities/Movie.cs
+++ src/Domain/Entities/Movie.cs
@@ -1,8 +1,6 @@
 ﻿using Domain.Entities.Base;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
 
 namespace Domain.Entities
 {
